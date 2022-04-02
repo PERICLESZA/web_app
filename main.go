07ed1541cb361112601ac21839a19508c9729f78
unknown <pericles1011@yahoo.com.br>
2022-04-02 16:05:28 -0300

    testes com organizacao do codigo

--- main.go
+++ main.go
@@ -1,138 +1,17 @@
 package main
 
 import (
-	"html/template"
-	"log"
 	"net/http"
 
-	"github.com/go-redis/redis"
-	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
-	"golang.org/x/crypto/bcrypt"
+	"./models"
+	"./routes"
+	"./utils"
 )
 
-//globals variables
-var client *redis.Client
-var store = sessions.NewCookieStore([]byte("t0p-s3cr3t"))
-var templates *template.Template
-
 func main() {
-	client = redis.NewClient(&redis.Options{Addr: "localhost:6379"})
-	templates = template.Must(template.ParseGlob("templates/*.html"))
-	r := mux.NewRouter()
-	r.HandleFunc("/", AuthRequired(indexGetHandler)).Methods("GET")
-	r.HandleFunc("/", AuthRequired(indexPostHandler)).Methods("POST")
-	r.HandleFunc("/contact", contactHandler).Methods("GET")
-	r.HandleFunc("/about", aboutHandler).Methods("GET")
-	r.HandleFunc("/", indexGetHandler).Methods("GET")
-	r.HandleFunc("/", indexPostHandler).Methods("POST")
-	r.HandleFunc("/login", loginGetHandler).Methods("GET")
-	r.HandleFunc("/login", loginPostHandler).Methods("POST")
-	r.HandleFunc("/register", registerGetHandler).Methods("GET")
-	r.HandleFunc("/register", registerPostHandler).Methods("POST")
-	fs := http.FileServer(http.Dir("./static/"))
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	models.Init()
+	utils.LoadTemplates("templates/*.html")
+	r := routes.NewRouter()
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":8001", nil))
-}
-
-func AuthRequired(handler http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		session, _ := store.Get(r, "session")
-		_, ok := session.Values["username"]
-		if !ok {
-			http.Redirect(w, r, "/login", 302)
-			return
-		}
-		handler.ServeHTTP(w, r)
-	}
-}
-
-//request index page handle
-func indexGetHandler(w http.ResponseWriter, r *http.Request) {
-	comments, err := client.LRange(client.Context(), "comments", 0, 10).Result()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-	templates.ExecuteTemplate(w, "index.html", comments)
-}
-
-//request index page POST handle
-func indexPostHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	//get the comment in html tag comment
-	comment := r.PostForm.Get("comment")
-	err := client.LPush(client.Context(), "comments", comment).Err()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	} //redirect to / when the submit form
-	http.Redirect(w, r, "/", 302)
-}
-
-func loginGetHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "login.html", nil)
-
-}
-
-func loginPostHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	username := r.PostForm.Get("username")
-	password := r.PostForm.Get("password")
-	hash, err := client.Get(client.Context(), "user: "+username).Bytes()
-	if err == redis.Nil {
-		templates.ExecuteTemplate(w, "login.html", "unknown user")
-		return
-	} else if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-
-	err = bcrypt.CompareHashAndPassword(hash, []byte(password))
-	if err != nil {
-		templates.ExecuteTemplate(w, "login.html", "invalid login")
-		return
-	}
-	session, _ := store.Get(r, "session")
-	session.Values["username"] = username
-	session.Save(r, w)
-	http.Redirect(w, r, "/", 302)
-}
-
-func registerGetHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "register.html", nil)
-}
-
-func registerPostHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	username := r.PostForm.Get("username")
-	password := r.PostForm.Get("password")
-	cost := bcrypt.DefaultCost
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-	err = client.Set(client.Context(), "user: "+username, hash, 0).Err()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("Internal server error"))
-		return
-	}
-	http.Redirect(w, r, "/login", 302)
-}
-
-//request contact page handle
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "contact.html", "This is the contact page!")
-}
-
-//request about page handle
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "about.html", "This is the about page!")
+	http.ListenAndServe(":8000", nil)
 }
---
+++ models/comment.go
@@ -0,0 +1,10 @@
+package models
+
+func GetComments() ([]string, error) {
+	return client.LRange(client.Context(), "comments", 0, 10).Result()
+
+}
+
+func PostComment(comment string) error {
+	return client.LPush(client.Context(), "comment", comment).Err()
+}
